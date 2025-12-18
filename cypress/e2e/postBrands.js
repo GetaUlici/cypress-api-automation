1e74022f4c854ca7175a16c696db2c1736b923fe
geta.ulici <ulicigeta@gmail.com>
2025-12-18 08:26:07 +0200

    add fakerjs

--- cypress/e2e/postBrands.js
+++ cypress/e2e/postBrands.js
@@ -1,18 +1,22 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 
 describe('Post Request', () => {
   it('Create a new brand via /brands api', () => {
-    const uniqueId = Date.now();
+    const name = faker.company.name();
+    const slug = faker.helpers.slugify(name.toLowerCase());
 
     cy.request({
       method: 'POST',
       url: '/brands',
       body: {
-        name: `brand-${uniqueId}`,
-        slug: `brand-${uniqueId}`,
+        name: name,
+        slug: slug,
       },
     }).then((response) => {
       expect(response.status).to.eql(201);
+      expect(response.body.name).to.eq(name);
+      expect(response.body.slug).to.eq(slug);
     });
   });
 });
